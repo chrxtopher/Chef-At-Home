f35a9c4bb57e7d09e9a595cc15197a54c59844c9
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-04 11:22:29 -0700

    Began building header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/header.css";
+
 function Header() {
   return (
-    <div>
-      <nav>header</nav>
-    </div>
+    <nav>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+
+      <ul>
+        <li>
+          <Link to="/">
+            <button>Meals</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <button>Bites</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <button>Desserts</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <button>Search</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
