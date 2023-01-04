2026e772ae5925a31c9e3e9dc2cd88f7f0d77c13
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-04 15:38:49 -0700

    Renders filter form for meals page

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 import Header from "../components/Header";
+import FilterRecipe from "../components/FilterRecipe";
 
 function Meals() {
   return (
     <div>
       <Header />
-      <h1>Meals Page</h1>
+      <h1>Meals</h1>
+      <p>
+        Whether you are cooking for one, impressing a date, or feeding the whole
+        family, there is a recipe here for you! Use the filters below to help
+        pin-point your next meal!
+      </p>
+      <FilterRecipe />
     </div>
   );
 }
