d867ce10a12c481952eed5ab2d7dd7dceb5b67c0
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-04 15:50:39 -0700

    Added all form elements

--- src/components/FilterRecipe.jsx
+++ src/components/FilterRecipe.jsx
@@ -2,18 +2,16 @@ import React from "react";
 
 function FilterRecipe() {
   return (
-    <form>
+    <form className="filter-form">
       <div className="form-group">
         <label>
           Vegan:
           <input type="checkbox" name="vegan" id="vegan"></input>
         </label>
-
         <label>
           Vegetarian:
           <input type="checkbox" name="vegetarian" id="vegetarian"></input>
         </label>
-
         <label>
           Keto Friendly:
           <input type="checkbox" name="keto" id="keto"></input>
@@ -24,12 +22,31 @@ function FilterRecipe() {
           Gluten Free:
           <input type="checkbox" name="gluten_free" id="gluten_free"></input>
         </label>
-
         <label>
           Dairy Free:
           <input type="checkbox" name="dairy_free" id="dairy_free"></input>
         </label>
       </div>
+      <select name="protein">
+        <option value="none">Select</option>
+        <option value="chicken">Chicken</option>
+        <option value="beef">Beef</option>
+        <option value="pork">Pork</option>
+        <option value="shrimp">Shrimp</option>
+        <option value="crab">Crab</option>
+        <option value="tuna">Tuna</option>
+        <option value="salmon">Salmon</option>
+      </select>
+      <label>
+        Serving Size:
+        <input
+          type="number"
+          name="serv_size"
+          id="serv_size"
+          min="1"
+          max="10"
+        ></input>
+      </label>
     </form>
   );
 }
