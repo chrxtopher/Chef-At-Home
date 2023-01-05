1ad76d413b5a1458aad3ed372397531d23ca6a23
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-04 17:18:50 -0700

    Adjusted some elements and added a submit button

--- src/components/FilterRecipe.jsx
+++ src/components/FilterRecipe.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../styles/filterForm.css";
+
 function FilterRecipe() {
   return (
     <form className="filter-form">
@@ -27,26 +29,35 @@ function FilterRecipe() {
           <input type="checkbox" name="dairy_free" id="dairy_free"></input>
         </label>
       </div>
-      <select name="protein">
-        <option value="none">Select</option>
-        <option value="chicken">Chicken</option>
-        <option value="beef">Beef</option>
-        <option value="pork">Pork</option>
-        <option value="shrimp">Shrimp</option>
-        <option value="crab">Crab</option>
-        <option value="tuna">Tuna</option>
-        <option value="salmon">Salmon</option>
-      </select>
+      <label>
+        Protein:
+        <select name="protein" disabled={false}>
+          <option value="none">Select</option>
+          <option value="chicken">Chicken</option>
+          <option value="beef">Beef</option>
+          <option value="pork">Pork</option>
+          <option value="shrimp">Shrimp</option>
+          <option value="crab">Crab</option>
+          <option value="tuna">Tuna</option>
+          <option value="salmon">Salmon</option>
+        </select>
+      </label>
+
       <label>
         Serving Size:
         <input
           type="number"
           name="serv_size"
           id="serv_size"
+          defaultValue="1"
           min="1"
-          max="10"
+          max="20"
+          className="num-input"
         ></input>
       </label>
+      <button type="submit" className="submit-button">
+        Search
+      </button>
     </form>
   );
 }
