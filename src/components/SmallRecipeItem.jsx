82808e5c1fb3fd5b905bdace8f88c7bb0acb31e8
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-06 23:14:42 -0700

    Added a view button
    This button will eventually lead to a page that will hold much more information for each recipe

--- src/components/SmallRecipeItem.jsx
+++ src/components/SmallRecipeItem.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "../styles/smallRecipeItem.css";
 
 function SmallRecipeItem({ id, title = "title", image }) {
+  const handleViewButton = (event) => {
+    event.preventDefault();
+    console.log(`${title} -- ${id}`);
+  };
+
   return (
     <div className="small-recipe-item">
       <h3>{title}</h3>
       <img src={image} alt={title} />
+      <button onClick={handleViewButton} className="small-recipe-button">
+        View
+      </button>
     </div>
   );
 }
