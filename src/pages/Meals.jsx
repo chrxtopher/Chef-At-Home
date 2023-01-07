592f635c08da33c2470bb29136c9172b8e3bc74d
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-06 23:14:58 -0700

    Began styling the meals page

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "../components/Header";
 import FilterRecipe from "../components/FilterRecipe";
+import "../styles/mealsPage.css";
 
 function Meals() {
   return (
