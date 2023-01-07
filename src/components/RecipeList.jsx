c8f6fc3e1134acac4d94e45b93a5549734da8ea7
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-06 21:34:46 -0700

    Implemented API functionality to this component to retrieve data

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { getFilteredRecipes } from "../utility/api";
 
 function RecipeList({ filterOptions = { options: "none" } }) {
-  const buttonClick = (event) => {
-    event.preventDefault();
-    console.log(filterOptions);
-  };
+  const [recipeItems, setRecipeItems] = useState([]);
+
+  useEffect(() => {
+    async function getRecipes() {
+      const response = await getFilteredRecipes(filterOptions);
+      setRecipeItems(response);
+      console.log(response);
+    }
+
+    // getRecipes();
+  }, [filterOptions]);
 
   return (
     <div>
       <h3>Recipe List</h3>
-      <button onClick={buttonClick}>Log Filters</button>
     </div>
   );
 }
