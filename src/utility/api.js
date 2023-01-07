ba82a4f837b6a56cb0ccf5c73cc6ff1e7c0b24da
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-07 00:33:25 -0700

    Adjusted the api fetch url to be a bit more specific

--- src/utility/api.js
+++ src/utility/api.js
@@ -1,13 +1,12 @@
 const BASE_URL = process.env.REACT_APP_BASE_API_URL;
 const API_KEY = process.env.REACT_APP_API_KEY;
-const CABOOSE = `apiKey=${API_KEY}`;
+const CABOOSE = `instructionsRequired=true&number=5&apiKey=${API_KEY}`;
 
 export async function getFilteredRecipes(filterObj = {}) {
   const diet = filterObj.diet.join();
   const dishType = filterObj.dishType.join();
-  const url = `${BASE_URL}complexSearch?diet=${diet}&type=${dishType}&titleMatch=${filterObj.protein}&number=5&${CABOOSE}`;
+  const url = `${BASE_URL}complexSearch?diet=${diet}&cuisine=${filterObj.cuisine}&type=${dishType}&includeIngredients=${filterObj.protein}&${CABOOSE}`;
 
   const response = await fetch(url).then((response) => response.json());
-  console.log(response);
   return response;
 }
