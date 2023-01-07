b206ae558bc78d415a3245d1c87f059a9b037b1b
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-06 19:14:26 -0700

    Now renders the recipe list component
    Also wrapped the form in a div for future styling

--- src/components/FilterRecipe.jsx
+++ src/components/FilterRecipe.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 import "../styles/filterForm.css";
+import RecipeList from "./RecipeList";
 
 function FilterRecipe({ dishType = ["main course"] }) {
   const defaultFilterObj = {
@@ -52,96 +53,100 @@ function FilterRecipe({ dishType = ["main course"] }) {
   /////////////////////////
 
   return (
-    <form className="filter-form" onSubmit={handleSubmit}>
-      <div className="form-group">
+    <div>
+      <form className="filter-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>
+            Vegan:
+            <input
+              type="checkbox"
+              name="vegan"
+              id="vegan"
+              value="vegan"
+              onChange={handleCheckboxChange}
+            ></input>
+          </label>
+          <label>
+            Vegetarian:
+            <input
+              type="checkbox"
+              name="vegetarian"
+              id="vegetarian"
+              value="vegetarian"
+              onChange={handleCheckboxChange}
+            ></input>
+          </label>
+          <label>
+            Keto Friendly:
+            <input
+              type="checkbox"
+              name="ketogenic"
+              id="keto"
+              value="ketogenic"
+              onChange={handleCheckboxChange}
+            ></input>
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Gluten Free:
+            <input
+              type="checkbox"
+              name="glutenFree"
+              id="glutenFree"
+              value="glutenFree"
+              onChange={handleCheckboxChange}
+            ></input>
+          </label>
+          <label>
+            Dairy Free:
+            <input
+              type="checkbox"
+              name="dairyFree"
+              id="dairyFree"
+              value="dairyFree"
+              onChange={handleCheckboxChange}
+            ></input>
+          </label>
+        </div>
         <label>
-          Vegan:
-          <input
-            type="checkbox"
-            name="vegan"
-            id="vegan"
-            value="vegan"
-            onChange={handleCheckboxChange}
-          ></input>
+          Protein:
+          <select
+            name="protein"
+            onChange={handleProteinChange}
+            disabled={filterObj.vegan || filterObj.vegetarian ? true : false}
+          >
+            <option value="">Select</option>
+            <option value="chicken">Chicken</option>
+            <option value="beef">Beef</option>
+            <option value="pork">Pork</option>
+            <option value="shrimp">Shrimp</option>
+            <option value="crab">Crab</option>
+            <option value="tuna">Tuna</option>
+            <option value="salmon">Salmon</option>
+          </select>
         </label>
-        <label>
-          Vegetarian:
-          <input
-            type="checkbox"
-            name="vegetarian"
-            id="vegetarian"
-            value="vegetarian"
-            onChange={handleCheckboxChange}
-          ></input>
-        </label>
-        <label>
-          Keto Friendly:
-          <input
-            type="checkbox"
-            name="ketogenic"
-            id="keto"
-            value="ketogenic"
-            onChange={handleCheckboxChange}
-          ></input>
-        </label>
-      </div>
-      <div className="form-group">
-        <label>
-          Gluten Free:
-          <input
-            type="checkbox"
-            name="glutenFree"
-            id="glutenFree"
-            value="glutenFree"
-            onChange={handleCheckboxChange}
-          ></input>
-        </label>
-        <label>
-          Dairy Free:
-          <input
-            type="checkbox"
-            name="dairyFree"
-            id="dairyFree"
-            value="dairyFree"
-            onChange={handleCheckboxChange}
-          ></input>
-        </label>
-      </div>
-      <label>
-        Protein:
-        <select
-          name="protein"
-          onChange={handleProteinChange}
-          disabled={filterObj.vegan || filterObj.vegetarian ? true : false}
-        >
-          <option value="">Select</option>
-          <option value="chicken">Chicken</option>
-          <option value="beef">Beef</option>
-          <option value="pork">Pork</option>
-          <option value="shrimp">Shrimp</option>
-          <option value="crab">Crab</option>
-          <option value="tuna">Tuna</option>
-          <option value="salmon">Salmon</option>
-        </select>
-      </label>
 
-      <label>
-        Serving Size:
-        <input
-          type="number"
-          name="servSize"
-          id="servSize"
-          defaultValue="1"
-          min="1"
-          max="20"
-          className="num-input"
-          onChange={handleServSizeChange}
-        ></input>
-      </label>
-      <button type="submit" className="submit-button">
-        Search
-      </button>
-    </form>
+        <label>
+          Serving Size:
+          <input
+            type="number"
+            name="servSize"
+            id="servSize"
+            defaultValue="1"
+            min="1"
+            max="20"
+            className="num-input"
+            onChange={handleServSizeChange}
+          ></input>
+        </label>
+        <button type="submit" className="submit-button">
+          Search
+        </button>
+      </form>
+
+      <RecipeList filterOptions={filterObj} />
+    </div>
   );
 }
 
