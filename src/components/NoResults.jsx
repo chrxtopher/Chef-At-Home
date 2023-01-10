3e51cbb379cbfab55d4d0ce1a7794971ded431c0
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-09 21:07:24 -0700

    Created a no results found component
    This component will be rendered after the user searches and there are no results found that fit their filter options

---
+++ src/components/NoResults.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NoResults() {
+  return (
+    <div className="no-results">
+      <h4>No Results Found</h4>
+      <p>Try adjusting your filter options.</p>
+    </div>
+  );
+}
+
+export default NoResults;
