008bc5a2845bc8f625703cfe7656c9bb4b605977
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-25 13:54:03 -0700

    Implemented filter form to Bites page

--- src/pages/Bites.jsx
+++ src/pages/Bites.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { motion as m } from "framer-motion";
 
 import Header from "../components/Header";
+import FilterRecipe from "../components/FilterRecipe";
 
 function Bites() {
   return (
@@ -14,7 +15,14 @@ function Bites() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <h1>Bites Page</h1>
+        <h1>Bites for Everyone</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <FilterRecipe dishType={["fingerfood"]} />
       </m.div>
     </div>
   );
