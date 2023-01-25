bf53d1d8491b6960802f08344065bcfd8e34bc40
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-01-25 13:53:34 -0700

    Added animate on scroll the each recipe item

--- src/components/SmallRecipeItem.jsx
+++ src/components/SmallRecipeItem.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavButton from "./NavButton";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import "../styles/smallRecipeItem.css";
 
 function SmallRecipeItem({ id, title = "title", image }) {
+  useEffect(() => {
+    Aos.init({});
+  }, []);
+
   return (
-    <div className="small-recipe-item">
+    <div className="small-recipe-item" data-aos="zoom-in">
       <h3>{title}</h3>
       <img src={image} alt={title} />
       <NavButton name="View" active={false} pageUrl={`recipe/${id}`} />
