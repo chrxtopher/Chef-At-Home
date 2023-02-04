39a4c20f0acdea85cdda2ff0cdbc1785d375222f
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-03 17:26:21 -0700

    Set up routing for the recipe page

--- src/pages/PagesAll.jsx
+++ src/pages/PagesAll.jsx
@@ -6,6 +6,7 @@ import Meals from "./Meals";
 import Bites from "./Bites";
 import Desserts from "./Desserts";
 import SearchRecipe from "./Search";
+import Recipe from "./Recipe";
 
 function PagesAll() {
   return (
@@ -15,6 +16,7 @@ function PagesAll() {
       <Route path="/bites" element={<Bites />} />
       <Route path="/desserts" element={<Desserts />} />
       <Route path="/search" element={<SearchRecipe />} />
+      <Route path="/recipe/:recipeId" element={<Recipe />} />
     </Routes>
   );
 }
