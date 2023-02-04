590c71c1cdc9bc696befc7946514e7dca0f9d73e
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-04 13:47:22 -0700

    completed styling of large recipe items

--- src/components/LargeRecipeItem.jsx
+++ src/components/LargeRecipeItem.jsx
@@ -10,15 +10,40 @@ function LargeRecipeItem({ recipe }) {
       transition={{ duration: 1 }}
       className="large-recipe-item"
     >
-      <div className="large-recipe-info">
+      <div className="large-recipe-top">
         <div className="large-recipe-img">
           <img src={recipe.image} alt={recipe.title} />
         </div>
-        <div className="large-recipe-information">
-          <p>{`Ready In ${recipe.readyInMinutes} Minutes`}</p>
-          <p>{`Preparation Time : ${recipe.preparationMinutes} Minutes`}</p>
-          <p>{`${recipe.extendedIngredients.length} Total Ingredients`}</p>
-          <p>{`Servings : ${recipe.servings}`}</p>
+        <div className="large-recipe-info">
+          <p>{`Ready In ${recipe.readyInMinutes} Minutes || Prep Time : ${recipe.preparationMinutes} Minutes`}</p>
+          <p>{`${recipe.servings} Servings || ${recipe.extendedIngredients.length} Total Ingredients`}</p>
+        </div>
+      </div>
+      <div className="large-recipe-bottom">
+        <div className="large-recipe-ingredients">
+          <h3>Ingredients</h3>
+          <ul>
+            {recipe.extendedIngredients.map((ingredient) => {
+              return (
+                <li key={ingredient.id}>
+                  <p>{`| ${ingredient.amount} ${ingredient.unit} ${ingredient.nameClean} |`}</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <hr />
+        <div className="large-recipe-instructions">
+          <h3>Instructions</h3>
+          <ul>
+            {recipe.analyzedInstructions[0].steps.map((step) => {
+              return (
+                <li key={step.number}>
+                  <p>{`${step.number}. ${step.step}`}</p>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </m.div>
