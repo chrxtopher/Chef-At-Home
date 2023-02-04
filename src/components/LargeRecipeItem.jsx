27b624dcb36b6811d69ec5a43a3cdee4ba434043
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-04 11:34:46 -0700

    Began styling large recipe items

--- src/components/LargeRecipeItem.jsx
+++ src/components/LargeRecipeItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { motion as m } from "framer-motion";
+import "../styles/largeRecipeItem.css";
 
 function LargeRecipeItem({ recipe }) {
   return (
@@ -13,7 +14,12 @@ function LargeRecipeItem({ recipe }) {
         <div className="large-recipe-img">
           <img src={recipe.image} alt={recipe.title} />
         </div>
-        <div className="large-recipe-information"></div>
+        <div className="large-recipe-information">
+          <p>{`Ready In ${recipe.readyInMinutes} Minutes`}</p>
+          <p>{`Preparation Time : ${recipe.preparationMinutes} Minutes`}</p>
+          <p>{`${recipe.extendedIngredients.length} Total Ingredients`}</p>
+          <p>{`Servings : ${recipe.servings}`}</p>
+        </div>
       </div>
     </m.div>
   );
