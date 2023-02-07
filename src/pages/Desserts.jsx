41f0632f75557738a5580f3c4307c680cc9b4aa1
Christopher Roddy <85670252+chrxtopher@users.noreply.github.com>
2023-02-07 16:41:48 -0700

    Update Desserts.jsx
    Added FilterRecipe to Desserts page

--- src/pages/Desserts.jsx
+++ src/pages/Desserts.jsx
@@ -3,6 +3,7 @@ import { motion as m } from "framer-motion";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import FilterRecipe from "../components/FilterRecipe";
 
 function Desserts() {
   return (
@@ -15,7 +16,14 @@ function Desserts() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1 }}
       >
-        <h1>Desserts Page</h1>
+        <h1>Indulge in Sweetness</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <FilterRecipe dishType={["dessert"]} />
       </m.div>
       <div>
         <Footer />
